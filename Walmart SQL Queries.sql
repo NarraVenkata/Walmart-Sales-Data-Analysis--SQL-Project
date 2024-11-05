0b8db94cc096887e4c38e6c787b40fcf952ba547
NarraVenkata <venkatanarra02@gmail.com>
2024-11-05 16:46:17 -0500

    Update Walmart SQL Queries.sql

--- Walmart SQL Queries.sql
+++ Walmart SQL Queries.sql
@@ -3,81 +3,84 @@ CREATE DATABASE IF NOT EXISTS walmart;
 USE walmart;
 
 CREATE TABLE sales(
-invoice_id VARCHAR(30) NOT NULL PRIMARY KEY,
-branch VARCHAR(5) NOT NULL,
-city VARCHAR(30) NOT NULL,
-customer_type VARCHAR(30) NOT NULL,
-gender VARCHAR(10) NOT NULL,
-product_line VARCHAR(100) NOT NULL,
-unit_price DECIMAL(10,2) NOT NULL,
-quantity INT(20) NOT NULL,
-vat FLOAT(6,4) NOT NULL,
-total DECIMAL(12, 4) NOT NULL,
-date DATETIME NOT NULL,
-time TIME NOT NULL,
-payment VARCHAR(15) NOT NULL,
+Invoice_ID VARCHAR(30) NOT NULL PRIMARY KEY,
+Branch VARCHAR(5) NOT NULL,
+City VARCHAR(30) NOT NULL,
+Customer_Type VARCHAR(30) NOT NULL,
+Gender VARCHAR(10) NOT NULL,
+Product_Line VARCHAR(100) NOT NULL,
+Unit_Price DECIMAL(10,2) NOT NULL,
+Quantity INT NOT NULL,
+Tax FLOAT NOT NULL,
+Total DECIMAL(12, 4) NOT NULL,
+Date DATETIME NOT NULL,
+Time TIME NOT NULL,
+Payment VARCHAR(15) NOT NULL,
 cogs DECIMAL(10,2) NOT NULL,
-gross_margin_pct FLOAT(11,9),
-gross_income DECIMAL(12, 4),
-rating FLOAT(2, 1)
+Gross_Margin_Percentage FLOAT,
+Gross_Income DECIMAL(12, 4),
+Rating FLOAT
 );
 
-SHOW GLOBAL VARIABLES LIKE 'local_infile';
-SET GLOBAL local_infile = 1;
 
-LOAD DATA LOCAL INFILE 
-'C:\GITHUBPROJECTS\Walmart-Sales-Data-Analysis--SQL-Project\WalmartSalesData.csv'
-INTO TABLE sales
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+BULK INSERT sales
+FROM 'C:\GITHUBPROJECTS\Walmart-Sales-Data-Analysis--SQL-Project\WalmartSalesData.csv'
+WITH (
+    FIELDTERMINATOR = ',',  
+    ROWTERMINATOR = '\n',   
+    FIRSTROW = 2            
+);
 
+
+select * from sales
 
 ------------------- Feature Engineering -----------------------------
 1. Time_of_day
 
 SELECT time,
 (CASE 
-	WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "Morning"
-	WHEN `time` BETWEEN "12:01:00" AND "16:00:00" THEN "Afternoon"
-	ELSE "Evening" 
+	WHEN time BETWEEN '00:00:00' AND '12:00:00' THEN 'Morning'
+	WHEN time BETWEEN '12:01:00' AND '16:00:00' THEN 'Afternoon'
+	ELSE 'Evening' 
 END) AS time_of_day
 FROM sales;
 
-ALTER TABLE sales ADD COLUMN time_of_day VARCHAR(20);
+
+ALTER TABLE sales ADD  Time_Of_Day VARCHAR(20);
 
 UPDATE sales
-SET time_of_day = (
+SET Time_Of_Day = (
 	CASE 
-		WHEN `time` BETWEEN "00:00:00" AND "12:00:00" THEN "Morning"
-		WHEN `time` BETWEEN "12:01:00" AND "16:00:00" THEN "Afternoon"
-		ELSE "Evening" 
-	END
+	WHEN time BETWEEN '00:00:00' AND '12:00:00' THEN 'Morning'
+	WHEN time BETWEEN '12:01:00' AND '16:00:00' THEN 'Afternoon'
+	ELSE 'Evening' 
+END
 );
 
 
 2.Day_name
 
 SELECT date,
-DAYNAME(date) AS day_name
+DATENAME(WeekDay,date) AS day_name
 FROM sales;
 
-ALTER TABLE sales ADD COLUMN day_name VARCHAR(10);
+ALTER TABLE sales ADD  Day_Name VARCHAR(10);
 
 UPDATE sales
-SET day_name = DAYNAME(date);
+SET Day_Name = DATENAME(WeekDay,date);
 
 3.Momth_name
 
 SELECT date,
-MONTHNAME(date) AS month_name
+DATENAME(MONTH,date) AS month_name
 FROM sales;
 
-ALTER TABLE sales ADD COLUMN month_name VARCHAR(10);
+ALTER TABLE sales ADD  Month_Name VARCHAR(10);
 
 UPDATE sales
-SET month_name = MONTHNAME(date);
+SET Month_Name = DATENAME(MONTH,date);
+
+select * from sales
 
 
 ----------------Exploratory Data Analysis (EDA)----------------------
@@ -92,13 +95,20 @@ Product Analysis
 -- 1.How many distinct product lines are there in the dataset?
 SELECT COUNT(DISTINCT product_line) FROM sales;
 
+--select distinct product_line from sales
+
 -- 2.What is the most common payment method?
+SELECT Top 1 payment, COUNT(payment) AS common_payment_method 
+FROM sales GROUP BY payment ORDER BY common_payment_method DESC;
+
+/*
 SELECT payment, COUNT(payment) AS common_payment_method 
-FROM sales GROUP BY payment ORDER BY common_payment_method DESC LIMIT 1;
+FROM sales GROUP BY payment ORDER BY common_payment_method DESC;
+*/
 
 -- 3.What is the most selling product line?
-SELECT product_line, count(product_Line) AS most_selling_product
-FROM sales GROUP BY product_line ORDER BY most_selling_product DESC LIMIT 1;
+SELECT  Top 1 product_line, count(product_Line) AS most_selling_product
+FROM sales GROUP BY product_line ORDER BY most_selling_product DESC
 
 -- 4.What is the total revenue by month?
 SELECT month_name, SUM(total) AS total_revenue
@@ -109,31 +119,33 @@ SELECT month_name, SUM(cogs) AS total_cogs
 FROM sales GROUP BY month_name ORDER BY total_cogs DESC;
 
 -- 6.Which product line generated the highest revenue?
-SELECT product_line, SUM(total) AS total_revenue
-FROM sales GROUP BY product_line ORDER BY total_revenue DESC LIMIT 1;
+SELECT top 1 product_line, SUM(total) AS total_revenue
+FROM sales GROUP BY product_line ORDER BY total_revenue DESC;
 
 -- 7.Which city has the highest revenue?
-SELECT city, SUM(total) AS total_revenue
-FROM sales GROUP BY city ORDER BY total_revenue DESC LIMIT 1;
+SELECT top 1 city, SUM(total) AS total_revenue
+FROM sales GROUP BY city ORDER BY total_revenue DESC;
 
--- 8.Which product line incurred the highest VAT?
-SELECT product_line, SUM(vat) as VAT 
-FROM sales GROUP BY product_line ORDER BY VAT DESC LIMIT 1;
+-- 8.Which product line incurred the highest Tax?
+SELECT top 1 product_line, SUM(Tax) as Tax 
+FROM sales GROUP BY product_line ORDER BY Tax DESC;
 
 -- 9.Retrieve each product line and add a column product_category, indicating 'Good' or 'Bad,'based on whether its sales are above the average.
 
-ALTER TABLE sales ADD COLUMN product_category VARCHAR(20);
+ALTER TABLE sales ADD Product_Category VARCHAR(20);
 
 UPDATE sales 
-SET product_category= 
+SET Product_Category = 
 (CASE 
-	WHEN total >= (SELECT AVG(total) FROM sales) THEN "Good"
-    ELSE "Bad"
+	WHEN total >= (SELECT AVG(total) FROM sales) THEN 'Good'
+    ELSE 'Bad'
 END)FROM sales;
 
+select * from sales
+
 -- 10.Which branch sold more products than average product sold?
-SELECT branch, SUM(quantity) AS quantity
-FROM sales GROUP BY branch HAVING SUM(quantity) > AVG(quantity) ORDER BY quantity DESC LIMIT 1;
+SELECT top 1 branch, SUM(quantity) AS quantity
+FROM sales GROUP BY branch HAVING SUM(quantity) > AVG(quantity) ORDER BY quantity DESC ;
 
 -- 11.What is the most common product line by gender?
 SELECT gender, product_line, COUNT(gender) total_count
@@ -153,16 +165,17 @@ SELECT day_name, time_of_day, COUNT(*) AS total_sales
 FROM sales WHERE day_name NOT IN ('Saturday','Sunday') GROUP BY day_name, time_of_day;
 
 -- 2.Identify the customer type that generates the highest revenue.
-SELECT customer_type, SUM(total) AS total_sales
-FROM sales GROUP BY customer_type ORDER BY total_sales DESC LIMIT 1;
+SELECT top 1 customer_type, SUM(total) AS total_sales
+FROM sales GROUP BY customer_type ORDER BY total_sales DESC;
 
 -- 3.Which city has the largest tax percent/ VAT (Value Added Tax)?
-SELECT city, SUM(VAT) AS total_VAT
-FROM sales GROUP BY city ORDER BY total_VAT DESC LIMIT 1;
+SELECT top 1 city, SUM(tax) AS total_tax
+FROM sales GROUP BY city ORDER BY total_tax DESC ;
 
--- 4.Which customer type pays the most in VAT?
-SELECT customer_type, SUM(VAT) AS total_VAT
-FROM sales GROUP BY customer_type ORDER BY total_VAT DESC LIMIT 1;
+-- 4.Which customer type pays the most in Tax?
+SELECT top 1 customer_type, SUM(tax) AS total_tax
+FROM sales GROUP BY customer_type ORDER BY total_tax DESC;
+
 
 Customer Analysis
 
@@ -173,27 +186,27 @@ SELECT COUNT(DISTINCT customer_type) FROM sales;
 SELECT COUNT(DISTINCT payment) FROM sales;
 
 -- 3.Which is the most common customer type?
-SELECT customer_type, COUNT(customer_type) AS common_customer
-FROM sales GROUP BY customer_type ORDER BY common_customer DESC LIMIT 1;
+SELECT top 1 customer_type, COUNT(customer_type) AS common_customer
+FROM sales GROUP BY customer_type ORDER BY common_customer DESC ;
 
 -- 4.Which customer type buys the most?
-SELECT customer_type, SUM(total) as total_sales
-FROM sales GROUP BY customer_type ORDER BY total_sales LIMIT 1;
+SELECT top 1 customer_type, SUM(total) as total_sales
+FROM sales GROUP BY customer_type ORDER BY total_sales ;
 
-SELECT customer_type, COUNT(*) AS most_buyer
-FROM sales GROUP BY customer_type ORDER BY most_buyer DESC LIMIT 1;
+SELECT top 1 customer_type, COUNT(*) AS most_buyer
+FROM sales GROUP BY customer_type ORDER BY most_buyer DESC 
 
 -- 5.What is the gender of most of the customers?
-SELECT gender, COUNT(*) AS all_genders 
-FROM sales GROUP BY gender ORDER BY all_genders DESC LIMIT 1;
+SELECT top 1 gender, COUNT(*) AS all_genders 
+FROM sales GROUP BY gender ORDER BY all_genders DESC;
 
 -- 6.What is the gender distribution per branch?
 SELECT branch, gender, COUNT(gender) AS gender_distribution
 FROM sales GROUP BY branch, gender ORDER BY branch;
 
 -- 7.Which time of the day do customers give most ratings?
-SELECT time_of_day, AVG(rating) AS average_rating
-FROM sales GROUP BY time_of_day ORDER BY average_rating DESC LIMIT 1;
+SELECT top 1 time_of_day, AVG(rating) AS average_rating
+FROM sales GROUP BY time_of_day ORDER BY average_rating DESC;
 
 -- 8.Which time of the day do customers give most ratings per branch?
 SELECT branch, time_of_day, AVG(rating) AS average_rating
@@ -201,11 +214,11 @@ FROM sales GROUP BY branch, time_of_day ORDER BY average_rating DESC;
 
 SELECT branch, time_of_day,
 AVG(rating) OVER(PARTITION BY branch) AS ratings
-FROM sales GROUP BY branch;
+FROM sales --GROUP BY branch;
 
 -- 9.Which day of the week has the best avg ratings?
-SELECT day_name, AVG(rating) AS average_rating
-FROM sales GROUP BY day_name ORDER BY average_rating DESC LIMIT 1;
+SELECT top 1 day_name, AVG(rating) AS average_rating
+FROM sales GROUP BY day_name ORDER BY average_rating DESC
 
 -- 10.Which day of the week has the best average ratings per branch?
 SELECT  branch, day_name, AVG(rating) AS average_rating
